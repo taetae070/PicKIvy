778c7c4f2797f83e442444b37894aa477d95fb1e
taetae <taetaeky07@gmail.com>
2024-08-22 17:30:28 +0900

    backend setting

---
+++ backend/src/index.ts
@@ -0,0 +1,21 @@
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// CORS 설정
+app.use(cors());
+
+// JSON 데이터 처리
+app.use(express.json());
+
+// 간단한 API 엔드포인트
+app.get('/api/hello', (req: Request, res: Response) => {
+  res.json({ message: 'Hello from the TypeScript backend!' });
+});
+
+// 서버 실행
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
---
+++ frontend/src/App.tsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+// import WelcomePage from './pages/WelcomePage';
+import WelcomePage from '@pages/WelcomePage';
+import HousePage from './pages/HousePage';
+import LoginModal from './components/LoginJoin/LoginModal';
+import LoginBtn from './components/LoginJoin/LoginBtn';
+import { BrowserRouter as Switch,Routes, Route } from "react-router-dom";
+// import { BrowserRouter as Router,Routes, Route } from "react-router-dom";
+
+function App() {
+  const [isModalOpen, setIsModalOpen] = useState (false);
+
+  const handleModalClose = ()=>{
+    setIsModalOpen(false);
+  }
+
+  const handleModalOpen = ()=>{
+    setIsModalOpen(true);
+  }
+
+  return (
+    <Switch>
+      <Routes>
+          <Route path="/" element={<WelcomePage />} />
+          <Route path="/house" element={<HousePage />} />
+          <Route path="/loginPage" element={<LoginModal onClose={handleCloseClick}/>} />
+        </Routes>
+    </Switch>
+        
+  );
+}
+
+
+export default App;

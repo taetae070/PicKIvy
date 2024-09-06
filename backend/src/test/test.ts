0554465ef8b09dee3930717a7cd5632c27dd7b1b
taetae <taetaeky07@gmail.com>
2024-09-06 23:30:21 +0900

    ing..

--- backend/src/test/test.ts
+++ backend/src/test/test.ts
@@ -1,10 +1,10 @@
 import request from 'supertest'; // HTTP 요청을 테스트하기 위해 사용
 import app from '../index'; // 서버 파일을 import
 
-describe('POST /api/hello', () => {
+describe('POST /api/users', () => {
   it('should return JSON response', async () => {
     const response = await request(app)
-      .post('/api/hello')
+      .post('/api/users')
       .send();
 
     // JSON 응답을 기대
